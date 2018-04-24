90ef271b7486ce601f1d76f78c8faefe3188f403
Robin Stocker <robin@nibor.org>
2018-04-24 23:15:15 +1000

    Add example

---
+++ benches/example.rs
@@ -0,0 +1,22 @@
+#[macro_use]
+extern crate criterion;
+
+use criterion::{Bencher, Criterion};
+
+fn example(b: &mut Bencher, s: &str) {
+    b.iter(|| s.len());
+}
+
+fn example_benchmark(c: &mut Criterion) {
+    c.bench_function_over_inputs(
+        "fun",
+        |b, s| example(b, s),
+        vec![
+            "foo",
+            "bar/baz"
+        ]
+    );
+}
+
+criterion_group!(benches, example_benchmark);
+criterion_main!(benches);
